b5c4202381284115cbc4b14b533b70c8020f2eb6
Caleb Jones <bionicle.dragon@gmail.com>
2014-09-06 21:02:35 -0400

    Initial Commit

---
+++ SuperHaxagon/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SuperHaxagon
+//
+//  Created by Caleb Jones on 9/6/14.
+//  Copyright (c) 2014 Caleb Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
